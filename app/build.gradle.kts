312a308ccf47d48da1255b89da90f4e40da9155e
Claus Rasmussen <decoid@gmail.com>
2024-07-07 00:13:15 +0200

    Set up signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("a1001albums.android.application")
     id("a1001albums.android.application.compose")
@@ -5,12 +8,21 @@ plugins {
     id("a1001albums.android.hilt")
 }
 
+val keystorePropertiesFile = rootProject.file("signing/secrets.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+fun getGitCommitCount(): Int {
+    val process = Runtime.getRuntime().exec("git rev-list --count HEAD")
+    return process.inputStream.bufferedReader().use { it.readText().trim().toInt() }
+}
+
 android {
     namespace = "dk.clausr.a1001albumsgenerator"
 
     defaultConfig {
         applicationId = "dk.clausr.a1001albumsgenerator"
-        versionCode = 1
+        versionCode = getGitCommitCount()
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -19,13 +31,26 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = rootProject.file("signing/Clausr.keystore")
+            storePassword = keystoreProperties["SIGNING_STORE_PASSWORD"] as String
+            keyAlias = keystoreProperties["GOOGLE_PLAY_SIGNING_KEY_ALIAS"] as String
+            keyPassword = keystoreProperties["SIGNING_KEY_PASSWORD"] as String
+        }
+    }
+
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
+        }
+        debug {
+            isMinifyEnabled = false
         }
     }
 
