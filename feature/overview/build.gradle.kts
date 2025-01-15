e2468990706d465909f876375f989da66257a13f
Claus Rasmussen <decoid@gmail.com>
2025-01-15 22:05:58 +0100

    smaller fixes here and there

--- feature/overview/build.gradle.kts
+++ feature/overview/build.gradle.kts
@@ -10,7 +10,6 @@ android {
 dependencies {
     implementation(project(":core:data-widget"))
     implementation(project(":widget"))
-    implementation(project(":core:analytics"))
 
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
