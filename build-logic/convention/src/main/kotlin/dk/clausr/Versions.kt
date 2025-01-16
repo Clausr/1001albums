a14e6f44920c903e1982b146ebbc4482a0c734db
Claus Rasmussen <decoid@gmail.com>
2025-01-16 23:06:59 +0100

    Update target api version to 35

--- build-logic/convention/src/main/kotlin/dk/clausr/Versions.kt
+++ build-logic/convention/src/main/kotlin/dk/clausr/Versions.kt
@@ -1,7 +1,7 @@
 package dk.clausr
 
 object Versions {
-    const val TargetVersion = 34
-    const val CompileSdk = 35
-    const val MinSdk = 26
+    const val TARGET_VERSION = 35
+    const val COMPILE_SDK = 35
+    const val MIN_SDK = 26
 }
