57746f07430606b90192726182632880a92b6318
Claus Rasmussen <decoid@gmail.com>
2025-08-30 21:44:24 +0200

    Make onboarding navigation type safe (#334)

--- feature/onboarding/src/main/java/dk/clausr/a1001albumsgenerator/onboarding/OnboardingDirections.kt
+++ feature/onboarding/src/main/java/dk/clausr/a1001albumsgenerator/onboarding/OnboardingDirections.kt
@@ -1,12 +1,12 @@
 package dk.clausr.a1001albumsgenerator.onboarding
 
-object OnboardingDirections {
-    object Routes {
-        internal const val ROOT = "onboarding"
-        internal const val PROJECT_NAME = "$ROOT/project"
-        internal const val STREAMING_PLATFORM = "$ROOT/streaming_platform"
-    }
+import androidx.navigation.NavController
+import kotlinx.serialization.Serializable
 
-    fun projectName() = Routes.PROJECT_NAME
-    fun streamingPlatform() = Routes.STREAMING_PLATFORM
-}
+@Serializable
+object ProjectNameRoute
+
+@Serializable
+object StreamingPlatformRoute
+
+internal fun NavController.navigateToStreamingPlatform() = navigate(StreamingPlatformRoute)
